449ae12a195364bb721fcdb100d82c4399a68e7d
손상일 <mrzero885@gmail.com>
2024-06-05 19:30:21 +0900

    snippet test init

---
+++ customSnippets.ts
@@ -0,0 +1,17 @@
+/**
+ * @prefix disclosureSnippet
+ * @description 디스클로져 자동생성 스니핏입니다.
+ * @code const { isOpen, onOpen, onClose } = useDisclosure();
+ */
+
+/**
+ * @prefix loglog
+ * @description 콘솔로그 스니펫
+ * @code console.log();
+ */
+
+/**
+ * @prefix useStateSnippet
+ * @description 디스클로져 자동생성 스니핏입니다.
+ * @code const [state,setState] = useState();
+ */
---
+++ tok-cli.config.ts
@@ -0,0 +1,22 @@
+import { RootConfig } from "@toktokhan-fe/cli";
+import { genTxt } from "./src/my-plugin";
+
+/**
+ * config 를 정의합니다.
+ *
+ * RootConfig 의 제네릭에 plugin 의 type 을 정의 함으로써 type safe 하게 option 을 정의할 수 있습니다.
+ */
+const config: RootConfig<{ plugins: [typeof genTxt] }> = {
+  /**
+   * tokript 가 해당 플러그인을 실행시키기 위해 조회하는 플러그인 리스트입니다.
+   */
+  plugins: [genTxt],
+  /**
+   * 정의된 이름을 key 값으로 config type 을 value 로써 config 정의가 가능합니다.
+   */
+  "gen:txt": {
+    output: "custom/path/my.txt",
+  },
+};
+
+export default config;
